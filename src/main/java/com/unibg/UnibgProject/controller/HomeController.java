66f2cebe5ad96d973ed56af9f797eb92bb554e84
saccocristian <100707100+saccocristian@users.noreply.github.com>
2023-11-27 21:36:47 +0100

    first commit: simple Spring project for testing

---
+++ src/main/java/com/unibg/UnibgProject/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.unibg.UnibgProject.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/welcome")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+    @GetMapping("/prova")
+    public String prova(){
+        return "Welcome";
+    }
+}
+
+/*
+@Controller
+public class SimpleController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
+ */
---
+++ src/test/java/com/unibg/UnibgProject/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibg.UnibgProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
