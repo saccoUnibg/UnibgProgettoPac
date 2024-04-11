87ad10ffa5b1d8817b0c697c43e505d6be2b25b4
saccocristian <100707100+saccocristian@users.noreply.github.com>
2024-04-11 14:59:28 +0200

    wip

--- src/main/java/com/unibg/UnibgProject/repository/CheckinRepository.java
+++ src/main/java/com/unibg/UnibgProject/repository/CheckinRepository.java
@@ -2,7 +2,11 @@ package com.unibg.UnibgProject.repository;
 
 import com.unibg.UnibgProject.Entity.CheckinEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CheckinRepository extends JpaRepository<CheckinEntity,Integer> {
+
+    @Query("delete from CheckinEntity c where c.id_prenotazione=idPrenotazione")
+    void deleteByIdPrenotazione(String idPrenotazione);
 
 }
